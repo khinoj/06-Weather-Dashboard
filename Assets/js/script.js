e149a4a377324b0af19dad9ec218419788798603
khinoj <kristopher.hinojosa@gmail.com>
2022-11-20 22:44:09 -0500

    update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,15 @@
 // grab text from button
-var presetcities = document.querySelector('#major-city')
 
 //grab text from input box
 
+//API_Key
+const openWeather_API_KEY = '5c36e0b211379adb9db1348735cb898b';
+
+
+city = "Atlanta";
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeather_API_KEY}`
+console.log(url)
+
+
+//city temp wind humidity
