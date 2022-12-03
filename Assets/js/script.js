28870c17ecf0574c1ec81b96cb5bca2e7bf34387
khinoj <kristopher.hinojosa@gmail.com>
2022-12-03 16:58:08 -0500

    update

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,22 @@
+//API_Key
+const openWeather_API_KEY = '5c36e0b211379adb9db1348735cb898b';
+let baseUrl = 'http://api.openweathermap.org/data/2.5/'
+let unitOfMeasure = 'imperial'
+city = ""
+let url
+
+
 // grab text from button
+
+
 
 //grab text from input box
 
-//API_Key
-const openWeather_API_KEY = '5c36e0b211379adb9db1348735cb898b';
 
 
-city = "Atlanta";
+// const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeather_API_KEY}`
+// console.log(url)
 
-const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeather_API_KEY}`
-console.log(url)
+$('#searchbtn').click(function () {
 
-
-//city temp wind humidity
+})
